a67882c9fda7faa4b76979cc4b8bf20bd703cea6
g4workshop <g4workshop@foxmail.com>
2012-03-28 21:46:30 +0800

    Full code first time commit

---
+++ WindowBaseApp/G4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  G4AppDelegate.h
+//  WindowBaseApp
+//
+//  Created by Dawen Rie on 12-3-25.
+//  Copyright (c) 2012年 G4 Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface G4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
---
+++ WindowBaseApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WindowBaseApp
+//
+//  Created by Dawen Rie on 12-3-25.
+//  Copyright (c) 2012年 G4 Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "G4AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([G4AppDelegate class]));
+    }
+}
